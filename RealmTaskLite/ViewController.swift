a41d766d7e14e626c1777ae5569efb31a57aa5a7
Warren Hansen <15slim@Warrens-MacBook-Pro.local>
2017-02-10 13:28:40 -0800

    task: setup complete

--- RealmTaskLite/ViewController.swift
+++ RealmTaskLite/ViewController.swift
@@ -7,8 +7,71 @@
 //
 
 import UIKit
+import RealmSwift
+
+// MARK: Model
+
+final class TaskList: Object {
+    dynamic var text = ""
+    dynamic var id = ""
+    let items = List<Task>()
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+final class Task: Object {
+    dynamic var text = ""
+    dynamic var completed = false
+}
 
 class ViewController: UITableViewController {
     
+    var items = List<Task>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        
+    }
+    
+    func setupUI() {
+        title = "My Tasks"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
+    }
+    
+    // MARK: UITableView
+    
+    override func tableView(_ tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.text
+        cell.textLabel?.alpha = item.completed ? 0.5 : 1
+        return cell
+    }
+    
+    // MARK: Functions
+    
+    func add() {
+        let alertController = UIAlertController(title: "New Task", message: "Enter Task Name", preferredStyle: .alert)
+        var alertTextField: UITextField!
+        alertController.addTextField { textField in
+            alertTextField = textField
+            textField.placeholder = "Task Name"
+        }
+        alertController.addAction(UIAlertAction(title: "Add", style: .default) { _ in
+            guard let text = alertTextField.text , !text.isEmpty else { return }
+            
+            self.items.append(Task(value: ["text": text]))
+            self.tableView.reloadData()
+        })
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
